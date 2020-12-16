3ca358024716b74fa6770fd1cc702747aed540f8
vagrant <fatima.ellabbar@gmail.com>
2020-12-16 18:48:50 +0000

    adding display tweets in main

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-  // --- our code goes here ---
 
+  // receiving the input event from the form
   $("#tweet-text").on('input',function(){
+    //count the number of the caracters 
     let counter = $("#tweet-text").val().length;
 
     if(counter <= 140){
+      // if the number below 140 remove the color red and print the counter in the output
       $('.counter').removeClass("change")
       $('.counter').val(counter);
     } else{
+       // if the number over 140 the color become red and the counter grows negative
       $('.counter').addClass("change")
       $('.counter').val(140-counter);
     }
