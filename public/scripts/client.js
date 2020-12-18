275abdd12001bd9780991175421475c3a5e3e819
vagrant <fatima.ellabbar@gmail.com>
2020-12-18 01:41:49 +0000

    fixing bugs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
 
   // get the time difference between now and the date of the creation of the tweet in minutes or secondes or hours or days or years.
   const timePassed = (time) => {
-    let Difference_In_Time = Date.now() - new Date(time)
-    let Difference_In_Secondes= Difference_In_Time / 1000
-    let Difference_In_Minutes = Difference_In_Time / (1000 * 60)
+    let Difference_In_Time = Date.now() - new Date(time);
+    let Difference_In_Secondes = Difference_In_Time / 1000;
+    let Difference_In_Minutes = Difference_In_Time / (1000 * 60);
     let Difference_In_Hours = Difference_In_Time / (1000 * 3600);
     let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-    let Difference_In_Years = Difference_In_Days /360
+    let Difference_In_Years = Difference_In_Days / 360;
     if (Difference_In_Years >= 1) {
       return Math.trunc(Difference_In_Years) + " years";
     } else if (Difference_In_Days >= 1) {
@@ -19,19 +19,19 @@ $(document).ready(function() {
     } else if (Difference_In_Minutes >= 1) {
       return Math.trunc(Difference_In_Minutes) + " minutes";
     } else {
-      return Math.trunc(Difference_In_Secondes) +" secondes"
+      return Math.trunc(Difference_In_Secondes) + " secondes";
     }
-  }
+  };
   // Preventing XSS with Escaping
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
   
   // create an article
   const createTweetElement = (tweet) => {
-    const element = 
+    const element =
       `<article>
           <header class="tweet-header">
             <div class="image">
@@ -47,14 +47,14 @@ $(document).ready(function() {
             <output> ${timePassed(tweet.created_at)}</output>
             <span class="emoji"> <i class="fa fa-flag"></i> <i class="fa fa-retweet"> </i> <i class="fa fa-heart"> </i>  </span>
           </footer>
-      </article>`
+      </article>`;
     return element;
-  }
+  };
 
   // add the new articles to the section in the page html
   const renderTweets = (tweets) => {
 
-    //loop throught the tweets 
+    //loop throught the tweets
     for (const tweet of tweets) {
 
       // create article for each tweet
@@ -62,29 +62,37 @@ $(document).ready(function() {
       //add the article to the DOM
       $('#tweets-container').prepend($tweet);
     }
-  }
+  };
  
-  // using AJAX to fetch tweets 
+  // using AJAX to fetch tweets
   const loadTweets = () => {
-     // create the url for the request
-     const url = `http://localhost:8080/tweets`;
+    // create the url for the request
+    const url = `http://localhost:8080/tweets`;
 
-     // Create an AJAX request GET
-     $.ajax({
-       method: 'GET', 
-       url: url,
-     })
-       .then((result) => {
-         // success. getting the result here
-         renderTweets(result);
-       })
-       .catch((err) => console.log(err));
-  }
+    // Create an AJAX request GET
+    $.ajax({
+      method: 'GET',
+      url: url,
+    })
+      .then((result) => {
+        // success. getting the result here
+        renderTweets(result);
+      })
+      .catch((err) => console.log(err));
+  };
 
   // on loading fetch the tweets and add them to the DOM
   loadTweets();
 
 
+  //Make the form slide up or down when the "Compose" button is clicked
+  $(".display").on('click', function() {
+    $(".new-tweet").slideToggle("slow");
+    // the textarea receive the focus
+    $("#tweet-text").focus();
+  });
+
+  // adding new tweet when submiting the form
   $('form').on('submit', function(event) {
 
     //prevent the default form submission behaviour
@@ -94,12 +102,8 @@ $(document).ready(function() {
     const url = `http://localhost:8080/tweets`;
 
 
-    //console.log($("<h5>").text($("#tweet-text").val()));
-
-
-
-    // if the tweet is not empty and below 140 cararcters post the tweet 
-    if ($("#tweet-text").val() && $("#tweet-text").val().length <=140) {
+    // if the tweet is not empty and below 140 cararcters post the tweet
+    if ($("#tweet-text").val() && $("#tweet-text").val().length <= 140) {
 
       $('.error').hide();
 
@@ -108,18 +112,18 @@ $(document).ready(function() {
 
       // Create an AJAX request POST
       $.ajax({
-          url: url,
-          type: "post",
-          data: values ,
-          success: function (response) {
-            $("#tweet-text").val("");
-            $('.counter').text(140);
-            $("#tweets-container").empty();
-            loadTweets();
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-            console.log(textStatus, errorThrown);
-          }
+        url: url,
+        type: "post",
+        data: values ,
+        success: function(response) {
+          $("#tweet-text").val("");
+          $('.counter').text(140);
+          $("#tweets-container").empty();
+          loadTweets();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log(textStatus, errorThrown);
+        }
       });
     } else if ($("#tweet-text").val() && $("#tweet-text").val().length > 140) {
       // show the error message
@@ -128,6 +132,6 @@ $(document).ready(function() {
     } else {
       $('.error').html(`&#9888 The tweet is empty please enter a tweet! &#9888`);
       $('.error').slideDown("slow");
-    } 
-  });   
+    }
+  });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
   // receiving the input event from the form
-  $("#tweet-text").on('input',function(){
-    //count the number of the caracters 
+  $("#tweet-text").on('input',function() {
+    //count the number of the caracters
     let counter = $("#tweet-text").val().length;
 
-    if(counter <= 140){
+    if (counter <= 140) {
       // if the number below 140 remove the color red and print the counter in the output
-      $('.counter').removeClass("change")
-      $('.counter').text(140-counter);
-    } else{
-       // if the number over 140 the color become red and the counter grows negative
-      $('.counter').addClass("change")
-      $('.counter').text(140-counter);
+      $('.counter').removeClass("change");
+      $('.counter').text(140 - counter);
+    } else {
+      // if the number over 140 the color become red and the counter grows negative
+      $('.counter').addClass("change");
+      $('.counter').text(140 - counter);
     }
    
-  })
+  });
 });
